b8768f835b270f3a26f3f58b1600dae94ddc04e9
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-11-20 14:34:16 -0500

    fixed email problem

--- server.js
+++ server.js
@@ -76,10 +76,6 @@ app.post('/', function(req, res) {
                         });
 });
 
-app.post('/refresh', function(req, res){
-     res.render('index');
-});
-
 //server listening
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
